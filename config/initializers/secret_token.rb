bb43c5a2800aabc10f3706eb85c831c66ea66853
mostlyclicks <carlos@mostlyclicks.com>
2013-08-27 10:48:37 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sbflander::Application.config.secret_key_base = '7f8d27228f617f6098b647c68b3c3dbd3c519133644e8990221d9b67be80af1dbb8e1973ae168f20d5dde24d4016d8ad1f6bbe62d45ba7640a6069005a906cad'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sbflander::Application.config.session_store :cookie_store, key: '_sbflander_session'
