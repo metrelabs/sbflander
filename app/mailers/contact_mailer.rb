22c6780af51f47d548d8a557f0d18759bfe37455
mostlyclicks <carlos@mostlyclicks.com>
2013-10-24 08:12:19 -0500

    changed to from address

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,7 +3,7 @@ class ContactMailer < ActionMailer::Base
   def contact_submission(contact)
   	@contact = contact
   	#Change mailto :to => "#{contact.from_lender}""
-  	mail(:to => "carlos@metreagency.com", :subject => "SBFConnection Form Submission", :from => "#{contact.from_lender}")
+  	mail(:to => "carlos@metreagency.com", :subject => "SBFConnection Form Submission", :from => "#{contact.email}")
   end
 
 end
