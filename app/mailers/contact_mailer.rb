56d2c03422b6319c9d19208abf25bf87acd848a7
mostlyclicks <carlos@mostlyclicks.com>
2013-10-24 11:26:45 -0500

    bcc to me

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,7 +3,7 @@ class ContactMailer < ActionMailer::Base
   def contact_submission(contact)
   	@contact = contact
   	#Change mailto :to => "#{contact.from_lender}""
-  	mail(:to => "#{contact.from_lender}",:cc => "carlos@metreagency.com", :subject => "SBFConnection Form Submission", :from => "#{contact.email}")
+  	mail(:to => "#{contact.from_lender}",:bcc => "carlos@metreagency.com", :subject => "SBFConnection Form Submission", :from => "#{contact.email}")
   end
 
 end
